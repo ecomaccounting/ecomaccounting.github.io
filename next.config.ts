687fc29fa86938bdaf9392e6122a0ab9c2f011e3
Vipul Mehta <vipul81@gmail.com>
2025-11-10 21:40:41 +0530

    gh-pages fix

--- next.config.ts
+++ next.config.ts
@@ -4,7 +4,7 @@ const nextConfig: NextConfig = {
   /* config options here */
   output: 'export', // Enables static HTML export
   images: { unoptimized: true },
-  assetPrefix: "/",
+  assetPrefix: "",
   basePath: "",
 };
 
