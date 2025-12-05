d7e2303fa3134534b9c35a9649e448906ee40df2
Vipul Mehta <vipul81@gmail.com>
2025-12-05 17:00:31 +0530

    UI fix

--- app/services/[slug]/page.tsx
+++ app/services/[slug]/page.tsx
@@ -28,7 +28,7 @@ export async function generateMetadata({ params }: { params: Promise<{ slug: str
   );
 
   if (!service) {
-    return { title: "Service Not Found | GPMJ & Associates" };
+    return { title: "Service Not Found" };
   }
 
   return {
@@ -57,21 +57,21 @@ export default async function ServiceDetailPage({
   if (!service) return notFound();
 
   return (
-    <section className="bg-gray-50 py-20">
+    <section className="py-20">
       <div className="container mx-auto px-6 md:px-12 max-w-4xl">
         {/* --- Header --- */}
         <header className="text-center mb-12">
           <h1 className="text-5xl md:text-5xl  mb-4">
             {service.name}
           </h1>
-          <p className="text-gray-600 text-lg max-w-3xl mx-auto">
+          <p className="text-lg max-w-3xl mx-auto">
             {service.text}
           </p>
         </header>
 
         {/* --- Highlights Section --- */}
-        <div className="bg-white shadow-md rounded-2xl p-8 md:p-10">
-          <h2 className="text-2xl font-semibold mb-6 text-gray-800 border-b pb-3">
+        <div className="bg-accent shadow-md rounded-2xl p-8 md:p-10">
+          <h2 className="text-2xl font-semibold mb-6 border-b pb-3">
             Key Highlights
           </h2>
 
@@ -79,9 +79,9 @@ export default async function ServiceDetailPage({
             {service.highlights.map((item, i) => (
               <li
                 key={i}
-                className="flex items-center gap-3 text-gray-700 font-medium"
+                className="flex items-center gap-3 font-medium"
               >
-                <span className="text-blue-600 text-xl">✔</span> {item}
+                <span className="text-xl">✔</span> {item}
               </li>
             ))}
           </ul>
